9997606d4443d789364c71284d89eb060c4c75fd
Petr Vích <petr@petrvich.eu>
2025-12-07 21:24:00 +0100

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'portal_app',
-      remotes: {
-        rentalGenerator: 'http://localhost:5001/assets/remoteEntry.js',
-      },
-      shared: ['react', 'react-dom']
-    })
-  ],
-  build: {
-    target: 'esnext'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'portal_app',
+        remotes: {
+          rentalGenerator: env.VITE_RENTAL_GENERATOR_URL || 'http://localhost:5001/assets/remoteEntry.js',
+        },
+        shared: ['react', 'react-dom']
+      })
+    ],
+    build: {
+      target: 'esnext'
+    }
   }
 })
